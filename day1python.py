a492d381cd021bdae3de33ff982636adcdfc4e8b
unknown <interiorsmart915@gmail.com>
2025-11-16 12:19:16 +0545

    add: class for employee

---
+++ day1python.py
@@ -0,0 +1,22 @@
+print('hello\nram')
+print('what \'s up')
+print('* * * * ')
+print('hello')
+
+print('*******')
+print('*     *')
+print('*     *')
+print('*******')
+print('''
+******
+*    *
+*    *
+******''')
+print('''
+           _
+          / _)
+  .-^^^- / /
+_/        /
+<_.|_|-|_|
+''')
+print("hello, world!")
